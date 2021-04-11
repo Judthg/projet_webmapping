9e821fae6d1709341130fe51641798c10c63b664
Judthg <81268256+Judthg@users.noreply.github.com>
2021-04-12 00:10:38 +0200

    test sidebar qui disparait

--- villes_historiques/script.js
+++ villes_historiques/script.js
@@ -607,6 +607,17 @@ map.on('load', function () {
     buildLocationList(villes);
 });
 
+//Sidebar immeubles histriques
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
 
 //Interactivité HOVER
 
